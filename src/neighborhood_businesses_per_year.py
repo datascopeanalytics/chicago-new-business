38cc792687a5ffada23da042592b6c307041b11b
Dean Malmgren <dean.malmgren@datascopeanalytics.com>
2015-08-28 08:22:30 -0500

    calculating the number of new  businesses in each neighborhood

---
+++ src/neighborhood_businesses_per_year.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+import sys
+import collections
+import csv
+
+import data
+
+def aggregate_neighborhood_by_year(filename, date_attr):
+    with open(filename) as stream:
+        reader = data.RawReader(stream)
+        counter = collections.defaultdict(collections.Counter)
+        for row in reader:
+            counter[row['NEIGHBORHOOD']][getattr(row, date_attr).year] +=1
+    return counter
+
+
+new_businesses = aggregate_neighborhood_by_year(sys.argv[1], 'date_issued')
+old_businesses = aggregate_neighborhood_by_year(sys.argv[2], 'expiration_date')
+
+writer = csv.writer(sys.stdout)
+writer.writerow(['neighborhood', 'year', 'new', 'old'])
+neighborhoods = list(set(new_businesses.keys() + old_businesses.keys()))
+neighborhoods.sort()
+for neighborhood in neighborhoods:
+    years = list(set(new_businesses[neighborhood].keys() + old_businesses[neighborhood].keys()))
+    years.sort()
+    for year in years:
+        writer.writerow([
+            neighborhood,
+            year,
+            new_businesses[neighborhood][year],
+            old_businesses[neighborhood][year],
+        ])
