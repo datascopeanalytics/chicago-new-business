2d67d875f96deb7270486693df91e3e26f75c162
JessicaFreaner <jessica.freaner@datascopeanalytics.com>
2016-05-04 11:49:16 -0500

    Filter biz data for Chicago, IL street addresses (no PO BOX!)

---
+++ src/filter_chicago_locations.py
@@ -0,0 +1,37 @@
+"""
+Output only rows for addresses in Chicago, IL.
+
+Excludes PO Boxes.
+"""
+# !/usr/bin/env python
+# -*- coding: utf8 -*-
+import sys
+import pandas as pd
+
+
+def drop_PO_box(df, check='BOX '):
+    """Filter PO Box addresses."""
+    return df[~df['ADDRESS'].str.contains(check)]
+
+
+def drop_outside_IL(df, check='IL'):
+    """Filter addresses outside IL."""
+    return df[df['STATE'].str.contains(check)]
+
+
+def drop_outside_Chicago(df, check='CHICAGO'):
+    """Filter addresses outside Chicago."""
+    return df[df['CITY'] == check]
+
+# load in business data
+df = pd.read_csv(sys.stdin, low_memory=False)
+# Filter addresses for only street address in Chicago, IL
+# with Lat | Lng
+# latlng_df = df[pd.notnull(df['LATITUDE'])].copy()
+# latlng_df = drop_PO_box(latlng_df)
+chicago_df = drop_PO_box(df)
+chicago_df = drop_outside_IL(chicago_df)
+chicago_df = drop_outside_Chicago(chicago_df)
+
+# SAVE OUTPUT
+chicago_df.to_csv(sys.stdout, index=False)
